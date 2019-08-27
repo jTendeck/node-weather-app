c3bca2d7341ff738ed0cae0eb12d88c7d9143526
Justin Tendeck <justin.tendeck@gmail.com>
2019-08-27 13:14:18 -0700

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (lat, long, callback) => {
+    const url = 'https://api.darksky.net/forecast/2e41574ae92695cfc8f957ca1a731d14/' + lat + ',' + long + '?units=si';
+
+    request({url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(undefined, 'Summary: ' + body.daily.data[0].summary + ' Temperature: '
+                + body.currently.temperature + '. Chance of Rain: '
+                + body.currently.precipProbability
+            )
+        }
+    })
+}
+
+module.exports = forecast;
